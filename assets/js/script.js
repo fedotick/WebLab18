4c3ee8ab1e928f81705f57c1bcd0c06b9c417990
fedotick <feodortcaciuc2004@gmail.com>
2024-03-25 11:18:37 +0200

    Task 4 complited

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,13 +23,13 @@ const synonyms = {
     'word3': 'synonym3',
     'word4': 'synonym4',
     'word5': 'synonym5'
-}
+};
 
 document.getElementById('inputTask3').addEventListener('input', function() {
     const word = this.value;
     
     let synonym;
-    
+
     if(Object.keys(synonyms).includes(word)){
         synonym = synonyms[word];
     } else {
@@ -38,3 +38,42 @@ document.getElementById('inputTask3').addEventListener('input', function() {
 
     document.getElementById('outputTask3').value = synonym;
 });
+
+// 4
+let city1 = {};
+
+city1.name = 'ГородN';
+city1.population = 1e6;
+
+let city2 = {
+    name: 'ГородМ',
+    population: 1e6,
+};
+
+let getName = function() {
+    return this.name;
+};
+
+city1.getName = getName;
+city2.getName = getName;
+
+let exportStr = function() {
+    let str;
+
+    for(variable in this) {
+        if (typeof(this[variable]) != 'function') {
+            str += `${variable}=${this[variable]}\n`;
+        }
+    }
+
+    return str;
+};
+
+city1.exportStr = exportStr;
+city2.exportStr = exportStr;
+
+console.log(city1.getName());
+console.log(city2.getName());
+
+console.log(city1.exportStr());
+console.log(city2.exportStr());

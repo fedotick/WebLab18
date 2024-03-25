ec444469ee2d051dd730c1c78b8bc61f7bf6c820
fedotick <feodortcaciuc2004@gmail.com>
2024-03-25 10:16:57 +0200

    Task 2 complited

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
 // 1
 document.getElementById('inputTask1').addEventListener('input', function() {
-    const mySet1 = new Set();
+    const mySet = new Set();
     const numbers = this.value.split(' ');
-    numbers.forEach(num => mySet1.add(num));
+    numbers.forEach(num => mySet.add(num));
 
-    document.getElementById('outputTask1').value = Array.from(mySet1);
+    document.getElementById('outputTask1').value = Array.from(mySet);
+});
+
+// 2
+document.getElementById('inputTask2').addEventListener('input', function() {
+    const mySet = new Set();
+    const words = this.value.split(' ');;
+    words.forEach(word => mySet.add(word));
+
+    document.getElementById('outputTask2').value = Array.from(mySet);
 });
